f5f086d5f8d4b91fdf9fcec20e32b6856f9aa23f
Zeynep Zülal <119702784+zeynepzulal@users.noreply.github.com>
2023-05-14 00:42:19 +0200

    Add files via upload

---
+++ helloworld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier:MIT
+// SPDX Licence -> Software Package Data Exchange
+
+
+// solidity compiler versiyonu 0.8.17 ye esit veya ondan büyük olmalidir diyor.
+pragma solidity ^0.8.17;
+
+//HelloWorld contractin ismi ,(biz verdik)
+contract HelloWorld {  
+   string public hello = "Hello World !!!";
+}
